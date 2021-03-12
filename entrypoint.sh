ec0a835edf013a9d80fab9acbb8ccce78567d98b
陳靖勳 <64603358+jing-shiun@users.noreply.github.com>
2021-03-12 08:59:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd /home/container
 
-# Output Current Java Version
-java -version ## only really needed to show what version is being used. Should be changed for different applications
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
