7fd49eed4b40673acb6a9064f0962bcdae16fd08
陳靖勳 <64603358+jing-shiun@users.noreply.github.com>
2021-03-13 21:19:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@ cd /home/container
 
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo ":/home/container$ npm start"
 
 # Run the Server
-${MODIFIED_STARTUP}
+npm start
